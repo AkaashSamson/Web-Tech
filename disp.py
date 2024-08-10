146db38572b02a0a31600e915cd5ef312958945d
AkaashSamson <akaashsam22@gmail.com>
2024-08-10 18:53:34 +0530

    text file and comments delete

--- disp.py
+++ disp.py
@@ -3,15 +3,6 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-# @app.route('/bye')
-# def bye():
-#     return "ok bye!"
-
-# #to greet a user
-# @app.route('/greet/<name>')
-# def greet(name):
-#     return "Hello, "+name
-   
 @app.route('/')
 def hello():
     return render_template('index.html')
