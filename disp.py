68751925e26c0d56490cfe907603cefb688b6f69
AkaashSamson <akaashsam22@gmail.com>
2024-08-22 20:11:10 +0530

    Jinja Test

--- disp.py
+++ disp.py
@@ -1,11 +1,16 @@
 from flask import Flask, render_template
+import random
+import datetime
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello():
-    return render_template('index.html')
+    rnum = random.randint(1, 100)
+    current_year = str(datetime.datetime.now().minute)
+    return render_template('index.html', num=rnum, cur_year=current_year)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
