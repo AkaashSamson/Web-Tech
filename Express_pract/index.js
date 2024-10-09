544ffedc9ef7a31e19dc78a674bfafc08fe106a8
AkaashSamson <akaashsam22@gmail.com>
2024-10-09 17:45:15 +0530

    /about navigation

--- Express_pract/index.js
+++ Express_pract/index.js
@@ -4,7 +4,15 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello World');
+    res.send('<h1>Hello World</h1>');
+    //console.log(req.rawHeaders);
+});
+
+app.get('/about', (req, res) => {
+    res.send('<h1>About Us</h1><p>This is a paragraph about us.</p>');
+});
+app.get('/contact', (req, res) => {
+    res.send('<h1>Contact Us</h1><p>You can contact us at contact@example.com.</p>');
 });
 
 app.listen(port, () => {
