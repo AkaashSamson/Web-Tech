eef108eaa2192b5c3baee8ebfb4524a0872b2abd
AkaashSamson <akaashsam22@gmail.com>
2024-10-17 05:10:39 +0530

    Routing and navigating

---
+++ Next/my-app/app/components/AddToCart.tsx
@@ -0,0 +1,11 @@
+'use client';
+import React from 'react'
+
+const AddToCart = () => {
+    return (
+        <div>            <button onClick={() => console.log('click')}>Buy</button>
+        </div>
+    )
+}
+
+export default AddToCart
--- Next/my-app/app/page.tsx
+++ Next/my-app/app/page.tsx
@@ -1,9 +1,13 @@
 import Image from "next/image";
+import Link from "next/link";
+import ProductCard from "./components/ProductCard";
 
 export default function Home() {
   return (
     <div>
       <h1>Hello From my 'next'</h1>
+      <Link href="/users">Users</Link>
+      <ProductCard />
     </div>
   )
 }
